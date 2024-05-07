713def408fab30f199834cf9bfc4269a2cc1a635
Anton Kochuev <exe_cutor@bk.ru>
2024-05-07 04:39:33 +0300

    sdfds

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def rules(request):
     template = 'pages/rules.html'
     return render(request, template)
+
 
 def about(request):
     template = 'pages/about.html'
